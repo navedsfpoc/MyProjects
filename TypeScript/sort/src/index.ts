3e2b2cc076745b917050c2632845de6c988eac06
Naved Ahmed <navedsmart@gmail.com>
2020-12-26 21:24:59 -0800

    26dec2020
    26dec2020

--- TypeScript/sort/src/index.ts
+++ TypeScript/sort/src/index.ts
@@ -1,22 +1,21 @@
-class Sorter {
-  constructor(public collection:number[]){}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-  sort():void{
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+// numbersCollection.sort();
+// console.log(numbersCollection.data);
 
-    const { length }= this.collection;
-    for (let i=0; i<length; i++ ){
-      for (let j =0; j <length -i -1;j++){
-        if(this.collection[j] > this.collection[j+1]){
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j+1];
-          this.collection[j+1] = leftHand;
+// const charactersCollection = new CharactersCollection('Xaayb');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
 
-        }
-      }
-    }
-  }
-}
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
 
-const sorter = new Sorter([10,3,-5,0]);
-sorter.sort();
-console.log(sorter.collection);
+linkedList.sort();
+linkedList.print();
